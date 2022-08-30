b1437797bf65cd958b926a235d9d5df66826edae
gladiador2 <rolonjose4@gmail.com>
2022-08-30 14:04:16 -0400

    modificar plantilla para proyecto de alex

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@ import morgan from "morgan";
 // Routes
 import usuariosRoutes from "./routes/usuario.routes";
 const app = express();
-
+const cors = require('cors');
 // Settings
 app.set("port", 4000);
-
+  
 // Middlewares
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(cors()); 
 
 // Routes
 app.use("/api/usuarios", usuariosRoutes);
